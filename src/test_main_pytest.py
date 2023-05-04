b5ad4dacdf2605b7866c8e2bac89b8966d83c0fb
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-05-04 17:17:58 +1000

    Added testing .py and .json file to test Add/Remove plant function

--- src/test_main_pytest.py
+++ src/test_main_pytest.py
@@ -1,6 +1,32 @@
 import pytest
+import json
+
 from schedule_functions import PlantSchedule
+from unittest.mock import patch
 
 
-file_name = "data.json"
+file_name = "test_data.json"
 
+def test_add_plant():
+    with patch('builtins.input', side_effect=["Monstera", "monthly", "2023-04-10", "300"]):
+        test_add = PlantSchedule(file_name)
+        test_add.add_plant()
+
+    with open(file_name) as f:
+        data = json.load(f)
+
+    assert len(data) == 1
+    assert data[0]["Name"] == "Monstera"
+    assert data[0]["Frequency"] == "monthly"
+    assert data[0]["Last_Watered"] == "2023-04-10"
+    assert data[0]["Water_Needed"] == "300"
+
+def test_remove_plant():
+    with patch('builtins.input', side_effect=["Monstera", "monthly", "2023-04-10", "300"]):
+        test_add = PlantSchedule(file_name)
+        test_add.remove_plant()
+    
+    with open(file_name) as f:
+        data = json.load(f)
+
+    assert len(data) == 2
