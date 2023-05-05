8805f24fb135ba3cb53adf97c11aee8b2310e45c
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-05-05 13:37:17 +1000

    Updated bash script

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-#! /bin/bash
+if [ -d "./.venv" ]; then
+    echo "The virtual environment already exists. Please wait a moment for it to activate..."
+    source .venv/bin/activate
+else
+    echo "The virtual environment does not exist. Please wait a moment for it to create and activate..."
+    python3 -m venv .venv
+    source .venv/bin/activate
+fi
 
-python3 -m venv .venv
-source .venv/bin/activate
 pip3 install -r requirements.txt 
 clear
 python3 main.py 
